08ab6e87f07f675b51afcf9192cb64e776df1022
hamdan ahmad <hamdanahamed426@gmail.com>
2025-12-05 10:05:00 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import "./styles.css";
+
+import { useState } from "react";
+
+export default function App() {
+  const [advice, setAdvice] = useState("");
+  async function getAdvice() {
+    const res = await fetch("https://api.adviceslip.com/advice");
+    const data = await res.json();
+    setAdvice(data.slip.advice);
+  }
+  return (
+    <>
+      <h1>{advice}</h1>
+      <button onClick={getAdvice}>click me for advice</button>
+    </>
+  );
+}
