7103f5b0eb52f0370890aab5821e27f2ffeb5c55
hamdan ahmad <hamdanahamed426@gmail.com>
2025-12-05 05:23:39 +0000

    styles added

--- src/App.js
+++ src/App.js
@@ -11,8 +11,28 @@ export default function App() {
   }
   return (
     <>
-      <h1>{advice}</h1>
-      <button onClick={getAdvice}>click me for advice</button>
+      <h1 className="advice">{advice}</h1>
+      <button className="btn" onClick={getAdvice}>click me for advice</button>
+      <p className="answer">
+        1&2.diffing is an algorithm used in reconsiliation which basically looks
+        for changes even tiniest bit in virtual DOM and updates it to real DOM
+        in js engine of respected browser. ie.. v8 engine in chrome
+        <br />
+        <br />
+        3.react is the frontend library of javascript for building user
+        interfaces and also it is component based and state driven library
+        created by jordan walke
+        <br />
+        <br />
+        4.transpiler converts from a version or language of program to another
+        version or another language of code which compatiblr for the machine or
+        compiler. eg. babel is used to convert es6 js to es5+ or below for older
+        browsers and it is also used in react to convert jsx to js
+        <br />
+        <br />
+        5.compiler turns compiles the source code to portable file which is
+        machine and can executed any time after the compilation
+      </p>
     </>
   );
 }
